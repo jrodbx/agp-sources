bf807a6718f45f40c4ffd9177f7194717d70aa40
Goooler <wangzongler@gmail.com>
2025-04-05 16:41:51 +0800

    Update 8.7.3 - 8.11.0-alpha03 sources
    Also fixes the Guava resolving issue:
    
    ```
    > Could not resolve all files for configuration ':agp8.10.0-rc01'.
       > Could not resolve com.google.guava:guava:33.3.1-jre.
         Required by:
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools:sdk-common:31.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.build:aaptcompiler:8.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.analytics-library:crash:31.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.analytics-library:shared:31.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.lint:lint-typedef-remover:31.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > androidx.databinding:databinding-compiler-common:8.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.build:builder-test-api:8.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.utp:android-test-plugin-result-listener-gradle-proto:31.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.build:gradle-api:8.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools:sdk-common:31.10.0-rc01 > com.android.tools:common:31.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.build:builder:8.10.0-rc01 > com.android.tools.analytics-library:tracker:31.10.0-rc01
             project : > com.android.tools.build:gradle:8.10.0-rc01 > com.android.tools.build:builder:8.10.0-rc01 > com.android.tools.build:apkzlib:8.10.0-rc01
          > Cannot choose between the following variants of com.google.guava:guava:33.3.1-android:
              - androidRuntimeElements
              - jreRuntimeElements
            All of them match the consumer attributes:
              - Variant 'androidRuntimeElements' capabilities com.google.collections:google-collections:33.3.1-android and com.google.guava:guava:33.3.1-android:
                  - Unmatched attributes:
                      - Provides org.gradle.category 'library' but the consumer didn't ask for it
                      - Provides org.gradle.dependency.bundling 'external' but the consumer didn't ask for it
                      - Provides org.gradle.jvm.environment 'android' but the consumer didn't ask for it
                      - Provides org.gradle.jvm.version '8' but the consumer didn't ask for it
                      - Provides org.gradle.libraryelements 'jar' but the consumer didn't ask for it
                      - Provides org.gradle.status 'release' but the consumer didn't ask for it
                      - Provides org.gradle.usage 'java-runtime' but the consumer didn't ask for it
              - Variant 'jreRuntimeElements' capabilities com.google.collections:google-collections:33.3.1-android and com.google.guava:guava:33.3.1-android:
                  - Unmatched attributes:
                      - Provides org.gradle.category 'library' but the consumer didn't ask for it
                      - Provides org.gradle.dependency.bundling 'external' but the consumer didn't ask for it
                      - Provides org.gradle.jvm.environment 'standard-jvm' but the consumer didn't ask for it
                      - Provides org.gradle.jvm.version '8' but the consumer didn't ask for it
                      - Provides org.gradle.libraryelements 'jar' but the consumer didn't ask for it
                      - Provides org.gradle.status 'release' but the consumer didn't ask for it
                      - Provides org.gradle.usage 'java-runtime' but the consumer didn't ask for it
    ```

---
+++ 8.7.3/com.android.tools/sdklib/com/android/sdklib/DeviceSystemImageMatcher.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.sdklib;
+
+import com.android.annotations.NonNull;
+import com.android.sdklib.devices.Device;
+import com.android.sdklib.repository.IdDisplay;
+
+import java.util.Collection;
+
+public final class DeviceSystemImageMatcher {
+
+    private DeviceSystemImageMatcher() {}
+
+    public static boolean matches(@NonNull Device device, @NonNull ISystemImage image) {
+        Collection<IdDisplay> tags = image.getTags();
+
+        if (!Device.isTablet(device)) {
+            if (tags.contains(SystemImageTags.TABLET_TAG)) {
+                return false;
+            }
+        }
+
+        Object id = device.getTagId();
+
+        if (id == null || id.equals(SystemImageTags.DEFAULT_TAG.getId())) {
+            return !SystemImageTags.isWearImage(tags)
+                    && !SystemImageTags.isDesktopImage(tags)
+                    && !SystemImageTags.isTvImage(tags)
+                    && !SystemImageTags.isAutomotiveImage(tags)
+                    && !tags.contains(SystemImageTags.CHROMEOS_TAG);
+        }
+
+        if (id.equals(SystemImageTags.ANDROID_TV_TAG.getId())
+                || id.equals(SystemImageTags.GOOGLE_TV_TAG.getId())) {
+            return SystemImageTags.isTvImage(tags);
+        }
+
+        return tags.stream().map(IdDisplay::getId).anyMatch(i -> i.equals(id));
+    }
+}
